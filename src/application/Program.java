76fd1785c7ccb3ab788c0db2ffabb28686e96b73
FabricioBuss <fabriciobuss@gmail.com>
2020-11-21 16:47:16 -0300

    update and delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -31,9 +34,23 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n==== TEST 04: seller insert ====");
+		/*System.out.println("\n==== TEST 04: seller insert ====");
 		Seller newSeller = new Seller(null, "Greg News", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = "+ newSeller.getId());
+		*/
+		System.out.println("\n==== TEST 05: seller update ====");
+		seller = sellerDao.findbyId(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
+		System.out.println("\n==== TEST 05: seller delete ====");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed!");
+		
+		sc.close();
 	}
 }
